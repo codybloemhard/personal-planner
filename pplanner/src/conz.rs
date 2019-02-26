7845cce15f0caecf8a380ecb27f917920fd5cdd9
Cody Bloemhard <cblmhrd@live.nl>
2019-02-26 22:59:19 +0000

    refactor: in new file

---
+++ pplanner/src/conz.rs
@@ -0,0 +1,65 @@
+use std::io;
+use std::io::Write; //flush stdout
+
+use termcolor::{ Color, ColorChoice, ColorSpec, StandardStream, WriteColor };
+
+pub struct Printer{
+    pub stream: StandardStream,
+    pub col: Color,
+}
+
+impl Printer {
+    pub fn set_color(&mut self, color: Color){
+        self.col = color;
+        self.stream.set_color(ColorSpec::new().set_fg(Some(color)))
+            .expect("Error: Printer > set_color > 0");
+    }
+
+    pub fn println(&mut self, msg: String){
+        writeln!(&mut self.stream, "{}", msg)
+            .expect("Error: Printer > println > 0");
+    }
+
+    pub fn print(&mut self, msg: String){
+        write!(&mut self.stream, "{}", msg)
+            .expect("Error: Printer > print > 0");
+    }
+
+    pub fn println_color(&mut self, msg: String, color: Color){
+        self.stream.set_color(ColorSpec::new().set_fg(Some(color)))
+            .expect("Error: Printer > println_color > 0");
+        writeln!(&mut self.stream, "{}", msg)
+            .expect("Error: Printer > println_color > 1");
+        self.stream.set_color(ColorSpec::new().set_fg(Some(self.col)))
+            .expect("Error: Printer > println_color > 2");
+    }
+
+    pub fn print_color(&mut self, msg: String, color: Color){
+        self.stream.set_color(ColorSpec::new().set_fg(Some(color)))
+            .expect("Error: Printer > print_color > 0");
+        write!(&mut self.stream, "{}", msg)
+            .expect("Error: Printer > print_color > 1");
+        self.stream.set_color(ColorSpec::new().set_fg(Some(self.col)))
+            .expect("Error: Printer > print_color > 2");
+    }
+}
+
+pub fn create_printer() -> Printer {
+    return Printer {
+        stream: StandardStream::stdout(ColorChoice::Always),
+        col: Color::White,
+    }
+}
+
+pub fn read_inp() -> String {
+    let mut inp = String::new();
+    io::stdin().read_line(&mut inp).expect("Error :reading line.");
+    return inp.trim().to_string();
+}
+
+pub fn prompt(printer: &mut Printer, msg : String) -> String {
+    printer.print_color(msg, Color::Cyan);
+    printer.stream.flush()
+        .expect("Error: Printer > println_color > 0");
+    return read_inp();
+}
--- pplanner/src/main.rs
+++ pplanner/src/main.rs
@@ -1,65 +1,12 @@
-use std::io;
-use std::io::Write; //flush stdout
 extern crate termcolor;
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
-struct Printer{
-    stream: StandardStream,
-    col: Color,
-}
+use termcolor::{ Color };
 
-impl Printer {
-    fn set_color(&mut self, color: Color){
-        self.col = color;
-        self.stream.set_color(ColorSpec::new().set_fg(Some(color)))
-            .expect("Error: Printer > set_color > 0");
-    }
+mod conz;
 
-    fn println(&mut self, msg: String){
-        writeln!(&mut self.stream, "{}", msg)
-            .expect("Error: Printer > println > 0");
-    }
-
-    fn print(&mut self, msg: String){
-        write!(&mut self.stream, "{}", msg)
-            .expect("Error: Printer > print > 0");
-    }
-
-    fn println_color(&mut self, msg: String, color: Color){
-        self.stream.set_color(ColorSpec::new().set_fg(Some(color)))
-            .expect("Error: Printer > println_color > 0");
-        writeln!(&mut self.stream, "{}", msg)
-            .expect("Error: Printer > println_color > 1");
-        self.stream.set_color(ColorSpec::new().set_fg(Some(self.col)))
-            .expect("Error: Printer > println_color > 2");
-    }
-
-    fn print_color(&mut self, msg: String, color: Color){
-        self.stream.set_color(ColorSpec::new().set_fg(Some(color)))
-            .expect("Error: Printer > print_color > 0");
-        write!(&mut self.stream, "{}", msg)
-            .expect("Error: Printer > print_color > 1");
-        self.stream.set_color(ColorSpec::new().set_fg(Some(self.col)))
-            .expect("Error: Printer > print_color > 2");
-    }
-}
-
-fn read_inp() -> String {
-    let mut inp = String::new();
-    io::stdin().read_line(&mut inp).expect("Error :reading line.");
-    return inp.trim().to_string();
-}
-
-fn prompt(printer: &mut Printer, msg : String) -> String {
-    printer.print_color(msg, Color::Cyan);
-    printer.stream.flush()
-        .expect("Error: Printer > println_color > 0");
-    return read_inp();
-}
-
-fn receive_command(printer: &mut Printer) {
+fn receive_command(printer: &mut conz::Printer) {
     loop{
-        let x = prompt(printer, String::from("cmd > "));
+        let x = conz::prompt(printer, String::from("cmd > "));
         match x.as_ref() {
             "q" => break,
             "quit" => break,
@@ -74,10 +21,7 @@ fn receive_command(printer: &mut Printer) {
 }
 
 fn main() {
-    let mut printer = Printer {
-        stream: StandardStream::stdout(ColorChoice::Auto),
-        col: Color::White,
-    };
+    let mut printer = conz::create_printer();
     printer.set_color(Color::Green);
     receive_command(&mut printer);
 }
