c8b569e9087bdf4c2afb287c03d928794a1a98b9
Cody Bloemhard <cblmhrd@live.nl>
2019-02-26 13:31:08 +0100

    prompt

--- pplanner/src/main.rs
+++ pplanner/src/main.rs
@@ -1,19 +1,31 @@
 use std::io;
+use std::io::Write; //flush stdout
 
 fn read_inp() -> String {
     let mut inp = String::new();
-    io::stdin().read_line(&mut inp).expect("Error reading line.");
+    io::stdin().read_line(&mut inp).expect("Error :reading line.");
     inp = inp.trim().to_string();
     return inp;
 }
 
-fn main() {
-    println!("Henlo Frens!");
+fn prompt(msg : &str) -> String {
+    print!("{}", msg);
+    io::stdout().flush().ok().expect("Error: stdout().flush() in fn prompt");
+    return read_inp();
+}
+
+fn receive_command() {
     loop{
-        let x = read_inp();
-        println!("{}", x);
-        if x == "q".to_string() {
-            break;
+        let x = prompt("cmd > ");
+        match x.as_ref() {
+            "q" => {break;},
+            "quit" => {break;},
+            _ => println!("Command [\"{}\"] not found!", x)
         }
     }
+}
+
+fn main() {
+    println!("Personal Planner");
+    receive_command();
 }
