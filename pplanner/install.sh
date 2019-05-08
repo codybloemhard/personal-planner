f1d1da7822f2570ec6e9112a32877472b32f4e8e
Cody Bloemhard <cblmhrd@live.nl>
2019-05-08 13:23:33 +0200

    ?

--- pplanner/install.sh
+++ pplanner/install.sh
@@ -3,5 +3,5 @@ cargo build --release
 sudo cp -f target/release/pplanner /usr/local/bin
 sudo chmod 755 /usr/local/bin/pplanner
 sudo mkdir -p /usr/local/share/man/man1
-sudo cp -f pplanner.groff /usr/local/share/man/man1/pplanner.1
+sudo cp -f pplanner.ms /usr/local/share/man/man1/pplanner.1
 sudo gzip /usr/local/share/man/man1/pplanner.1
