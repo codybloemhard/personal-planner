82f6790d1e612b2f71ecd52f88f6738139edc68e
Cody Bloemhard <cblmhrd@live.nl>
2019-05-08 13:18:48 +0200

    basic groff man page

--- pplanner/install.sh
+++ pplanner/install.sh
@@ -2,5 +2,6 @@ sudo mkdir -p /usr/local/bin
 cargo build --release
 sudo cp -f target/release/pplanner /usr/local/bin
 sudo chmod 755 /usr/local/bin/pplanner
-#sudo mkdir -p /usr/local/man
-#sudo chmod 644 /usr/local/share/man/man1/st.1
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp -f pplanner.groff /usr/local/share/man/man1/pplanner.1
+sudo gzip /usr/local/share/man/man1/pplanner.1
